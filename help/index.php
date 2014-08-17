926084eca9ea382c99f0054f7ecad167ed565288
simxitnsz <seth_thoenen@hotmail.com>
2014-08-17 12:41:40 -0500

    Added images for guides.

--- help/index.php
+++ help/index.php
@@ -1,111 +1,187 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<link rel="stylesheet" href="../css/reset.css" type="text/css" />
-<link rel="stylesheet" href="../css/stylesheet.css" type="text/css" />
-<link rel="stylesheet" href="../css/print.css" type="text/css" media="print" />
-<script type="text/javascript" src="../js/jquery-1.4.2.js"></script>
-<script type="text/javascript" src="../js/slickBox.js"></script>
-    
-<title>Resnet :: Tutorials</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="../css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="../css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="../css/print.css" type="text/css" media="print" />
+        <script type="text/javascript" src="../js/jquery-1.4.2.js"></script>
+        <script type="text/javascript" src="../js/slickBox.js"></script>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        <title>Resnet :: Tutorials</title>
 
-  ga('create', 'UA-2196753-3', 'missouristate.edu');
-  ga('send', 'pageview');
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-2196753-3', 'missouristate.edu');
+            ga('send', 'pageview');
+            
+        </script>
 
-</script>
+    </head>
+    <body>
 
-</head>
-<body>
+        <?php include_once("../includes/navBar_secondary.inc.php") ?>
 
-<?php include_once("../includes/navBar_secondary.inc.php") ?>
-		
-	<div id = "wrapper">
-<?php include_once("../includes/header_secondary.inc.php") ?>
+        <div id="wrapper">
+            <?php include_once("../includes/header_secondary.inc.php") ?>
+            <div class="fourColumn">
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/printerIcon.png" alt="Printers" width="75" height="75" /><br />
+                   Printers
+                    </a>
+                    <a class="second-link" href="../help/printerSetup.php#WINVISTA7PRINT">
+                        Windows 7
+                    </a>
+                    <a class="second-link" href="../help/printerSetup.php#WIN8PRINT">
+                        Windows 8 and 8.1
+                    </a>
+                    <a class="second-link" href="../help/printerSetup.php#LEOPARDPRINT">
+                        Mac OS X (10.5, 10.6)
+                    </a>
+                    <a class="second-link" href="../help/printerSetup.php#LIONPRINT">
+                        Mac OS X (10.7, 10.8, 10.9)
+                    </a>
+                    <a class="second-link" href="../help/printerSetup.php#UBUNTUPRINT">
+                        Ubuntu
+                    </a>
+                </div>
 
-<div id = "leftColumn">
-            	<h2>Guides and Tutorials</h2>
-                 <ul class="superPlain">
-                	<li>
-       					<h3>General</h3>
-       					<ul>
-                    		<li><a href="printerSetup.php" title="">Connecting to Front Desk Printers</a></li>
-	       					<li><a href="recom.php" title="">Computer Recommendations</a></li>
-	       					<li><a href="netShares.php" title="">Connecting to Network Shares</a></li>
-	      					<li><a href="SkydriveSetup.php" title="">Connecting to the Skydrive</a></li>
-	       					<li> <a href="#" id="slick-toggle_a"><img src="../images/plus.gif" alt="+" /></a>
-                        	 	 <a href="mailClient.php" title="">Mail Client Configuration</a> 
-                       	
-                       
-							<div id="slickbox_a" class="slickBox">
-								<ul>
-									<li><a href="bearmailandroid.php">Android E-mail client</a></li>
-                                	<li><a href="bearmail-iPhone.php">Apple iPhone / iPod Touch Configuration</a></li>
-									<li><a href="bearmailApple.php">Apple Mail Configuration</a></li>
-									<li><a href="bearmailBlackBerry.php">BlackBerry Configuration</a></li>
-									<li><a href="bearmailEntourage.php">Microsoft Entourage 2008</a></li>
-									<li><a href="bearmailOutlook.php">Microsoft Outlook 2003 Configuration</a></li>
-									<li><a href="bearmailOutlook2007-10.php">Microsoft Outlook 2007 - 2010 Configuration</a></li>
-									<li><a href="bearmailThunder.php">Mozilla Thunderbird Configuration</a></li>
-									<li><a href="bearmailOperaMail.php">Opera Mail Configuration</a></li>
-									<li><a href="bearmailWindowsLiveMail.php">Windows Live Mail</a></li>
-									<li><a href="bearmailWinPhone8.php">Windows Phone 8</a></li>
-								</ul>
-                            </div>
-	      				 </li>
-        			</ul>
-                </li>
-   				<li>
-        	<h3>Network Setup and Troubleshooting</h3>
-       			<ul>
-					<li><a href="resreg.php">Registering Your computer</a></li>
-                	<li><a href="cable.php">Choosing the Right Cable</a></li>
-    	   			<li><a href="dhcp.php">Configuring your DHCP</a></li>
-    	 			<li><a href="findMACid.php">How to Find Your MAC Address (XP/Vista/OSX)</a></li>
-    	  			<li><a href="other-appletv.php">How to Find Your MAC Address (AppleTV)</a></li>
-	      			<li><a href="#" id="slick-toggle_b"><img src="../images/plus.gif" alt="+" /></a>
-                    	<a href="consoles.php">How to Register and Configure your Game Console</a> 
-                   
-                    
-                   
-	       			<div id="slickbox_b" class="slickBox">
-			 			<ul>
-                        	<li><a href="consoles-xbox.php">Microsoft XBox Configuration</a></li>
-							<li><a href="consoles-xbox360.php">Microsoft XBox 360 Configuration</a></li>
-							<li><a href="consoles-gc.php">Nintendo Gamecube Configuration</a></li>
-							<li><a href="consoles-wii.php">Nintendo Wii Configuration</a></li>
-							<li><a href="consoles-ps2.php">Sony PS2 Configuration</a></li>
-							<li><a href="consoles-ps3.php">Sony PS3 Configuration</a></li>
-			  			</ul>
-                    </div>
-	       			</li>
-	       				<li><a href="../wireless/index.php">Connecting to the Wireless Network</a></li>
-        			</ul>
-                  </li>
-    			  <li>
-        		  <h3>Computer Security</h3>
-        		  <ul>
-                  	<li><a href="autoUpdate.php">Setting up Automatic Updates</a></li>
-					<li><a href="firewallOn.php">Setting up Your Firewall</a></li>
-					<li><a href="manUpdate.php">Installing Updates Manually</a></li>
-       			 </ul>
-           		 </li>
-				</ul>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/windowsLogo.png" alt="Windows" width="75" height="75" /><br />
+                   Windows
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                </div>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/wifiIcon.png" alt="WiFi" width="75" height="75" /><br />
+                   Networks
+                    </a>
+                    <a class="second-link" href="../wireless/index.php">
+                        WiFi Setup
+                    </a>
+                    <a class="second-link" href="../help/resreg.php">
+                        Wired Registration
+                    </a>
+                    <a class="second-link" href="../help/netShares.php">
+                        Network Shares
+                    </a>
+                    <a class="second-link" href="../help/findMACid.php">
+                        Finding A MAC Address (PC/Mac)
+                    </a>
+                </div>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/appleLogo.png" alt="Apple" width="75" height="75" /><br />
+                   Apple
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                </div>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/emailIcon.png" alt="Mail" width="75" height="75" /><br />
+                   Mail
+                    </a>
+                    <a class="second-link" href="http://resnet.missouristate.edu/help/bearmailandroid.php">
+                        Android
+                    </a>
+                    <a class="second-link" href="http://resnet.missouristate.edu/help/bearmail-iPhone.php">
+                        iOS
+                    </a>
+                    <a class="second-link" href="http://resnet.missouristate.edu/help/bearmailApple.php">
+                        Apple Mail
+                    </a>
+                    <a class="second-link" href="http://resnet.missouristate.edu/help/bearmailOutlook2007-10.php">
+                        Outlook
+                    </a>
+                    <a class="second-link" href="http://resnet.missouristate.edu/help/mailClient.php">
+                        Additional Email Tutorials
+                    </a>
+                </div>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/linuxLogo.png" alt="Linux" width="75" height="75" /><br />
+                   Linux
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                </div>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/consoleIcon.png" alt="Entertainment & Media" width="75" height="75" /><br />
+                   Entertainment & Media
+                    </a>
+                    <a class="second-link" href="">
+                        Xbox One
+                    </a>
+                    <a class="second-link" href="">
+                        PlayStation 4
+                    </a>
+                    <a class="second-link" href="">
+                        AppleTV
+                    </a>
+                    <a class="second-link" href="">
+                        Other Consoles
+                    </a>
+                    <a class="second-link" href="">
+                        Other Media Streaming Devices
+                    </a>
+                </div>
+                <div class="slide-btn">
+                    <a class="first-link" href=""><img src="../images/tutorials/mobileIcon.png" alt="Mobile" width="75" height="75" /><br />
+                   Mobile
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                    <a class="second-link" href="">
+                        Demo
+                    </a>
+                </div>
             </div>
- 			<div id = "rightColumn">
- 			<?php include_once("../includes/sidePanel_tutorials.inc.php") ?>
- 			</div>
-			<?php include_once("../includes/footer.inc.php") ?>
-    </div>
+
+
+            <?php include_once("../includes/footer.inc.php") ?>
+        </div>
 
 
 
-</body>
+    </body>
 </html>

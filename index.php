2181356121c41935ba219e433c605513869465d6
Chris Young <cy335@live.missouristate.edu>
2014-10-10 15:16:14 -0500

    Removed mobile site redirect.

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<?php
-    require_once('mobile_device_detect.php');
-    $mobile = mobile_device_detect();
-    
-    // redirect all mobiles to mobile site and all other browsers to desktop site
-    if($mobile==true){
-      header('Location:http://resnet.missouristate.edu/mobile');
-    }
-    
-?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>

f4f1e059b7fae5e87167edb94960b221f3599a8d
simxitnsz <seth_thoenen@hotmail.com>
2014-12-06 01:02:28 -0600

    Added sidebar to tutorial's page.

--- help/index.php
+++ help/index.php
@@ -22,166 +22,174 @@
         </script>
 
     </head>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'];
+    ?>
     <body>
 
-        <?php include_once("../includes/navBar_primary.inc.php") ?>
+        <?php include_once($path . "/includes/navBar_primary.inc.php") ?>
 
         <div id="wrapper">
-            <?php include_once("../includes/header_primary.inc.php") ?>
-            <div class="fourColumn">
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/printerIcon.png" alt="Printers" width="75" height="75" /><br />
-                   Printers
-                    </a>
-                    <a class="second-link" href="../help/printerSetupWin7.php">
+            <?php include_once($path . "/includes/header_primary.inc.php") ?>
+            <div id="LeftNav">
+                <?php include_once($path . "/includes/LeftNav.php"); ?>
+            </div>
+            <div id="Main">
+                <div class="threeColumn">
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/printerIcon.png" alt="Printers" width="75" height="75" /><br />
+                            Printers
+                        </a>
+                        <a class="second-link" href="../help/printerSetupWin7.php">
                         Windows 7
-                    </a>
-                    <a class="second-link" href="../help/printerSetupWin8.php">
+                        </a>
+                        <a class="second-link" href="../help/printerSetupWin8.php">
                         Windows 8 and 8.1
-                    </a>
-                    <a class="second-link" href="../help/printerSetupMacLegacy.php">
+                        </a>
+                        <a class="second-link" href="../help/printerSetupMacLegacy.php">
                         Mac OS X (10.5 & 10.6)
-                    </a>
-                    <a class="second-link" href="../help/printerSetupMacModern.php">
+                        </a>
+                        <a class="second-link" href="../help/printerSetupMacModern.php">
                         Mac OS X (10.7 - 10.9)
-                    </a>
-                    <a class="second-link" href="../help/printerSetupMacYosemite.php">
+                        </a>
+                        <a class="second-link" href="../help/printerSetupMacYosemite.php">
                         Mac OS X (10.10)
-                    </a>
-                    <a class="second-link" href="../help/printerSetupUbuntu.php">
+                        </a>
+                        <a class="second-link" href="../help/printerSetupUbuntu.php">
                         Ubuntu
-                    </a>
-                </div>
+                        </a>
+                    </div>
 
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/windowsLogo.png" alt="Windows" width="75" height="75" /><br />
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/windowsLogo.png" alt="Windows" width="75" height="75" /><br />
                    Windows
-                    </a>
-                    <a class="second-link" href="../wireless/resnet40-Win8.php">
+                        </a>
+                        <a class="second-link" href="../wireless/resnet40-Win8.php">
                         WiFi
-                    </a>
-                    <a class="second-link" href="../help/printerSetup.php#WIN8PRINT">
+                        </a>
+                        <a class="second-link" href="../help/printerSetup.php#WIN8PRINT">
                         Printers
-                    </a>
-                    <a class="second-link" href="../help/findMACid.php#VISTA">
+                        </a>
+                        <a class="second-link" href="../help/findMACid.php#VISTA">
                         Finding Your MAC Address
-                    </a>
-                    <a class="second-link" href="../help/bearmailWindowsLiveMail.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmailWindowsLiveMail.php">
                         Windows Live Mail
-                    </a>
-                </div>
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/wifiIcon.png" alt="WiFi" width="75" height="75" /><br />
+                        </a>
+                    </div>
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/wifiIcon.png" alt="WiFi" width="75" height="75" /><br />
                    Networks
-                    </a>
-                    <a class="second-link" href="../wireless/index.php">
+                        </a>
+                        <a class="second-link" href="../wireless/index.php">
                         WiFi Setup
-                    </a>
-                    <a class="second-link" href="../help/resreg.php">
+                        </a>
+                        <a class="second-link" href="../help/resreg.php">
                         Wired Registration
-                    </a>
-                    <a class="second-link" href="../help/netShares.php">
+                        </a>
+                        <a class="second-link" href="../help/netShares.php">
                         Network Shares
-                    </a>
-                    <a class="second-link" href="../help/findMACid.php">
+                        </a>
+                        <a class="second-link" href="../help/findMACid.php">
                         Finding A MAC Address (PC/Mac)
-                    </a>
-                </div>
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/appleLogo.png" alt="Apple" width="75" height="75" /><br />
+                        </a>
+                    </div>
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/appleLogo.png" alt="Apple" width="75" height="75" /><br />
                    Apple
-                    </a>
-                    <!-- <a class="second-link" href="">
-                        WiFi
-                    </a> -->
-                    <a class="second-link" href="../help/printerSetup.php#LIONPRINT">
+                        </a>
+                                                    <!-- <a class="second-link" href="">
+                                                                                                                                    WiFi
+                                                                                                                                </a> -->
+                        <a class="second-link" href="../help/printerSetup.php#LIONPRINT">
                         Printers
-                    </a>
-                    <a class="second-link" href="../help/findMACid.php#OSX">
+                        </a>
+                        <a class="second-link" href="../help/findMACid.php#OSX">
                         Finding Your MAC Address
-                    </a>
-                    <a class="second-link" href="../help/bearmailApple.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmailApple.php">
                         Configuring Apple Mail
-                    </a>
-                </div>
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/emailIcon.png" alt="Mail" width="75" height="75" /><br />
+                        </a>
+                    </div>
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/emailIcon.png" alt="Mail" width="75" height="75" /><br />
                    Mail
-                    </a>
-                    <a class="second-link" href="../help/bearmailandroid.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmailandroid.php">
                         Android
-                    </a>
-                    <a class="second-link" href="../help/bearmail-iPhone.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmail-iPhone.php">
                         iOS
-                    </a>
-                    <a class="second-link" href="../help/bearmailApple.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmailApple.php">
                         Apple Mail
-                    </a>
-                    <a class="second-link" href="../help/bearmailOutlook2007-10.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmailOutlook2007-10.php">
                         Outlook
-                    </a>
-                    <a class="second-link" href="../help/mailClient.php">
+                        </a>
+                        <a class="second-link" href="../help/mailClient.php">
                         Additional Email Tutorials
-                    </a>
-                </div>
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/mobileIcon.png" alt="Mobile" width="75" height="75" /><br />
+                        </a>
+                    </div>
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/mobileIcon.png" alt="Mobile" width="75" height="75" /><br />
                    Mobile
-                    </a>
-                    <a class="second-link" href="../wireless/android.php">
+                        </a>
+                        <a class="second-link" href="../wireless/android.php">
                         Android WiFi
-                    </a>
-                    <a class="second-link" href="../wireless/resnet40-iphone.php">
+                        </a>
+                        <a class="second-link" href="../wireless/resnet40-iphone.php">
                         iOS WiFi
-                    </a>
-                    <a class="second-link" href="../help/bearmailandroid.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmailandroid.php">
                         Android Email
-                    </a>
-                    <a class="second-link" href="../help/bearmail-iPhone.php">
+                        </a>
+                        <a class="second-link" href="../help/bearmail-iPhone.php">
                         iOS Email
-                    </a>
-                </div>
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/consoleIcon.png" alt="Entertainment & Media" width="75" height="75" /><br />
+                        </a>
+                    </div>
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/consoleIcon.png" alt="Entertainment & Media" width="75" height="75" /><br />
                    Entertainment & Media
-                    </a>
-                    <a class="second-link" href="../help/consoles-xbox360.php">
+                        </a>
+                        <a class="second-link" href="../help/consoles-xbox360.php">
                         Xbox 360
-                    </a>
-                    <a class="second-link" href="../help/consoles-ps3.php">
+                        </a>
+                        <a class="second-link" href="../help/consoles-ps3.php">
                         PlayStation 3
-                    </a>
-                    <!-- <a class="second-link" href="">
-                        Xbox One
-                    </a>
-                    <a class="second-link" href="">
-                        PlayStation 4
-                    </a> -->
-                    <a class="second-link" href="../help/other-appletv.php">
+                        </a>
+                                                    <!-- <a class="second-link" href="">
+                                                                                                                                    Xbox One
+                                                                                                                                </a>
+                                                                                                                                <a class="second-link" href="">
+                                                                                                                                    PlayStation 4
+                                                                                                                                </a> -->
+                        <a class="second-link" href="../help/other-appletv.php">
                         AppleTV
-                    </a>
-                    <a class="second-link" href="../help/consoles.php">
+                        </a>
+                        <a class="second-link" href="../help/consoles.php">
                         Other Consoles
-                    </a>
-                    <!-- <a class="second-link" href="">
-                        Other Media Streaming Devices
-                    </a> -->
-                </div>
-                <div class="slide-btn">
-                    <a class="first-link" href=""><img src="../images/tutorials/gears.png" alt="Other" width="75" height="75" /><br />
+                        </a>
+                                                    <!-- <a class="second-link" href="">
+                                                                                                                                    Other Media Streaming Devices
+                                                                                                                                </a> -->
+                    </div>
+                    <div class="slide-btn">
+                        <a class="first-link" href=""><img src="../images/tutorials/gears.png" alt="Other" width="75" height="75" /><br />
                    Other
-                    </a>
-                    <a class="second-link" href="../help/printerSetup.php#UBUNTUPRINT">
+                        </a>
+                        <a class="second-link" href="../help/printerSetup.php#UBUNTUPRINT">
                         Linux Printers
-                    </a>
-                    <a class="second-link" href="../help/recom.php">
+                        </a>
+                        <a class="second-link" href="../help/recom.php">
                         Computer Recommendations
-                    </a>
-                    <a class="second-link" href="../help/netShares.php">
+                        </a>
+                        <a class="second-link" href="../help/netShares.php">
                         Connect To Network Shares
-                    </a>
+                        </a>
+                    </div>
                 </div>
-                </div>
+            </div>
             <?php include_once("../includes/footer.inc.php") ?>
         </div>
 

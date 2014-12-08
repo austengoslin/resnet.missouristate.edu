616f4ce2d33002865b5d0b890cf6e0983bd624c7
simxitnsz <seth_thoenen@hotmail.com>
2014-12-08 16:03:33 -0600

    Updated aup.php

--- aup.php
+++ aup.php
@@ -1,131 +1,116 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<link rel="stylesheet" href="css/reset.css" type="text/css" />
-<link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
-<link rel="stylesheet" href="css/print.css" type="text/css" media="print" />
-<title>Resnet :: Acceptable Use Policy</title>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="css/print.css" type="text/css" media="print" />
+        <title>Resnet :: Acceptable Use Policy</title>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-2196753-1', 'missouristate.edu');
-  ga('send', 'pageview');
-
-</script>
-
-</head>
-
-<body>
-	<?php include_once("includes/navBar_primary.inc.php"); ?>
-		
-	<div id = "wrapper">
-	<?php include_once("includes/header_primary.inc.php"); ?>
-
-			<!-- Content -->
-		  <div id = "leftColumn">
-			<h2>ResNet Acceptable Use Policy</h2>
-				<p class = "paragraph">
-					Missouri State University provides access to the campus network in residence hall living units. 
-					This privilege is assisted by funds from not-for-profit organizations (MOREnet), and is meant to 
-  					be used equitably and fairly by all to assist students, faculty, and staff in fulfilling the academic programs of Missouri State University. 
-					In no way can Missouri State network resources be used for commercial gain.
-				</p>
-                <p class = "paragraph">
-					The Computer Services Department maintains the network through its <a href="http://networking.missouristate.edu">Networking Services</a> division, 
-                    and has developed many policies and procedures to facilitate the equitable use of this valuable resource. 
-					These policies are available at <a href="http://www.missouristate.edu/ais/itpolicies.htm">http://www.missouristate.edu/ais/itpolicies.htm</a>.
-					ResNet's acceptable use policy is meant to supplement not supplant these policies and procedures. 
-					It is Residence Life, Housing and Dining Services&#8217; expectation that students familiarize themselves with and abide by Computer Services policies and procedures 
-                    as well as all local, state, and federal law.
-				</p>
-			    <p class = "paragraph">
-					In addition to these policies, Residence Life, Housing and Dining Services has additional expectations to help us all be better stewards of networking bandwidth.
-				</p>
-				<ul>
-					<li>
-						<p class = "answer">
-							Transferring of copyrighted materials is<strong> STRICTLY</strong> prohibited.
-							Should a student sharing copyrighted material be reported to ResNet, and the student's illegal activity be confirmed, the student will be notified 
-                            of the infraction and asked promptly to rectify the situation. 
-							A record will be made of the infraction and the student will be submitted to student conduct for further action. 
-							Multiple infractions may lead to termination of students&#8217; access privileges.
-						</p>
-				   </li>
-          		  <li>
-					<p class = "answer">
-						The use of routers, gateways, hubs, switches or bridges when connected to Missouri State&#8217;s Ethernet jacks is prohibited without prior approval. 
-						This includes wireless devices, such as Bluetooth, 802.11 devices and Apple's airports. 
-						Approval is limited to academic necessity, and then only routers. 
-						Since ample jacks are provided to students, approval is HIGHLY unlikely. 
-						Students may use routers set up privately without connection to the school's network for any reason. 
-						In no instance may wireless devices be used as they interfere with school resources by operating in an unregulated frequency range.
-					</p>
-
-				</li>
-                <li>
-					<p class = "answer">
-						Students are expected to be proactive in virus protection efforts. 
-						To assist students, Residence Life will provide assistance in locating a free antivirus program to use while you reside in a residence hall. 
-						In addition to anti-virus software, students should make sure they adequately patch their software and operating systems for security holes.
-					</p>
-				</li>
-                <li>
-					<p class = "answer">
-						Students are expected to refrain from attempting to access any resources on the network that they do not have prior explicit permission to use. 
-						This includes, but is not limited to, "browsing" the network to see if there are open shares on the network.
-					</p>
-				</li>
-                <li>
-					<p class = "answer">
-						Students are expected to be responsible for their own machines and computer accounts. 
-						You are not to share computer accounts with	anyone and are strictly responsible for any action taken by others using your machine. 
-						While not a frequent occurrence, people with malevolent intentions will attempt to use others to try and deflect attention from themselves. 
-						Users should not allow themselves to become victims.
-					</p>
-				</li>
-                <li>
-					<p class = "answer">
-						Students should be aware of the software they use and the configuration of their computers. 
-						Advance operating systems and networking software can be complicated programs with many "security" holes. 
-						Students should spend time learning how to properly use the software they download and the operating systems they use.
-					</p>
-				</li>
-			</ul>
-			<p>
-			Violation of any of the policies covering the use of computers and the campus network may result in the immediate deactivation of your Ethernet jack until the situation is 
-            resolved. Serious offenders risk permanent shutdown, referral to the student conduct system and/or criminal prosecution.
-			</p>
-	  </div>
-            <!-- //end content -->
-
-         	
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             
+            ga('create', 'UA-2196753-1', 'missouristate.edu');
+            ga('send', 'pageview');
+            
+        </script>
 
-	
-			<div id="rightColumn">
-    	<div class="contactButton"> 
-        	<a href="contact.php"><img class="aligncenter" src="images/contactUsButtonText.png" alt="Contact Us"/></a>
-            <p class="post-meta">(417) 836-6100  |  <a href="/contact.php" title="contact us">resnet@missouristate.edu</a></p> 
+    </head>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'];
+    ?>
+    <body>
+        <?php include_once($path . "/includes/navBar_primary.inc.php"); ?>
+
+        <div id="wrapper">
+            <?php include_once($path . "/includes/header_primary.inc.php"); ?>
+
+            <!-- Content -->
+            <div id="Main">
+                <h2>ResNet Acceptable Use Policy</h2>
+                <p class="paragraph">
+					Missouri State University provides access to the campus network in residence hall living units.
+					This privilege is assisted by funds from not-for-profit organizations (MOREnet), and is meant to
+  					be used equitably and fairly by all to assist students, faculty, and staff in fulfilling the academic programs of Missouri State University.
+					In no way can Missouri State network resources be used for commercial gain.
+                </p>
+                <p class="paragraph">
+					The Computer Services Department maintains the network through its <a href="http://networking.missouristate.edu">Networking Services</a> division,
+                    and has developed many policies and procedures to facilitate the equitable use of this valuable resource.
+					These policies are available at <a href="http://www.missouristate.edu/ais/itpolicies.htm">http://www.missouristate.edu/ais/itpolicies.htm</a>.
+					ResNet's acceptable use policy is meant to supplement not supplant these policies and procedures.
+					It is Residence Life, Housing and Dining Services&#8217; expectation that students familiarize themselves with and abide by Computer Services policies and procedures
+                    as well as all local, state, and federal law.
+                </p>
+                <p class="paragraph">
+					In addition to these policies, Residence Life, Housing and Dining Services has additional expectations to help us all be better stewards of networking bandwidth.
+                </p>
+                <ul>
+                    <li>
+                        <p class="answer">
+							Transferring of copyrighted materials is<strong> STRICTLY</strong> prohibited.
+							Should a student sharing copyrighted material be reported to ResNet, and the student's illegal activity be confirmed, the student will be notified
+                            of the infraction and asked promptly to rectify the situation.
+							A record will be made of the infraction and the student will be submitted to student conduct for further action.
+							Multiple infractions may lead to termination of students&#8217; access privileges.
+                        </p>
+                    </li>
+                    <li>
+                        <p class="answer">
+						The use of routers, gateways, hubs, switches or bridges when connected to Missouri State&#8217;s Ethernet jacks is prohibited without prior approval.
+						This includes wireless devices, such as Bluetooth, 802.11 devices and Apple's airports.
+						Approval is limited to academic necessity, and then only routers.
+						Since ample jacks are provided to students, approval is HIGHLY unlikely.
+						Students may use routers set up privately without connection to the school's network for any reason.
+						In no instance may wireless devices be used as they interfere with school resources by operating in an unregulated frequency range.
+                        </p>
+
+                    </li>
+                    <li>
+                        <p class="answer">
+						Students are expected to be proactive in virus protection efforts.
+						To assist students, Residence Life will provide assistance in locating a free antivirus program to use while you reside in a residence hall.
+						In addition to anti-virus software, students should make sure they adequately patch their software and operating systems for security holes.
+                        </p>
+                    </li>
+                    <li>
+                        <p class="answer">
+						Students are expected to refrain from attempting to access any resources on the network that they do not have prior explicit permission to use.
+						This includes, but is not limited to, "browsing" the network to see if there are open shares on the network.
+                        </p>
+                    </li>
+                    <li>
+                        <p class="answer">
+						Students are expected to be responsible for their own machines and computer accounts.
+						You are not to share computer accounts with	anyone and are strictly responsible for any action taken by others using your machine.
+						While not a frequent occurrence, people with malevolent intentions will attempt to use others to try and deflect attention from themselves.
+						Users should not allow themselves to become victims.
+                        </p>
+                    </li>
+                    <li>
+                        <p class="answer">
+						Students should be aware of the software they use and the configuration of their computers.
+						Advance operating systems and networking software can be complicated programs with many "security" holes.
+						Students should spend time learning how to properly use the software they download and the operating systems they use.
+                        </p>
+                    </li>
+                </ul>
+                <p>
+			Violation of any of the policies covering the use of computers and the campus network may result in the immediate deactivation of your Ethernet jack until the situation is
+            resolved. Serious offenders risk permanent shutdown, referral to the student conduct system and/or criminal prosecution.
+                </p>
+            </div>
+            <!-- //end content -->
+            <div id="LeftNav">
+                <?php include_once($path . "/includes/LeftNav.php"); ?>
+            </div>
+            <?php include_once("includes/footer.inc.php") ?>
         </div>
-       
-       <h3 class="textHeader">Free Anti-Virus</h3>
-       <p>
-       <a href="http://www.iantivirus.com/"><img src="images/iantivirus-logo.png" alt="Anti-Virus for Mac" /></a>
-       <a href="http://www.microsoft.com/security_essentials/"><img src="images/microsoft-security-essentials.png" alt="Anti-Virus for Windows" /></a>
-       </p>
-
-     
-     </div>
-	<?php include_once("includes/footer.inc.php") ?>
-    </div>
 
 
 
-</body>
+    </body>
 </html>

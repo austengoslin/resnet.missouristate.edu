03df67654b7705251bd3a7d168610dab5b4ffcbf
simxitnsz <seth_thoenen@hotmail.com>
2014-12-20 15:35:57 -0600

    moved BlackBerry tutorial.

---
+++ help/Mail/BlackBerry.php
@@ -0,0 +1,92 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="/css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="/css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="/css/print.css" type="text/css" media="print" />
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-2196753-3', 'missouristate.edu');
+            ga('send', 'pageview');
+            
+        </script>
+
+        <title>Resnet :: Tutorials ::  Mail Clients :: BlackBerry Email Setup</title>
+    </head>
+        </head>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'];
+    ?>
+    <body>
+        <?php include_once($path . "/includes/navBar_primary.inc.php"); ?>
+
+        <div id="wrapper">
+            <?php include_once($path . "/includes/header_primary.inc.php"); ?>
+
+            <div id="Main">
+                <div id="tutorial">
+                    <!-- start content -->
+                    <h2>BlackBerry Email Setup</h2>
+
+                    <p class="answer"><strong>Note:</strong> BlackBerry Internet Service, which is included with an active data plan,
+                is required in order to configure email.
+                    </p>
+
+
+
+
+                    <ol class="indent">
+                        <li>Select "Setup Wizard" &raquo; "Email Setup"</li>
+                        <li>Select "I want to create or add an email address" and click "Next".</li>
+                        <li>Enter the following information and select "Next":
+                            <ul class="answer">
+                                <li>Email address: <strong>BearPass Email@missouristate.edu</strong></li>
+                                <li>Password: <strong>Missouri State Password</strong></li>
+                            </ul>
+                        </li>
+                        <li>The message "We were unable to set up 'BearPass Email@missouristate.edu'. Choose one of the following options and select next," should be displayed.</li>
+                        <li>Select "I will provide the settings to add this email account" and click "Next".</li>
+                        <li>Select "Internet Service Provider Email (POP/IMAP)" and click "Next".</li>
+                        <li>On the screen "I will provide the settings to add this email account", accept the setting and click "Next".</li>
+                        <span class="indent">Enter the following information and select "Next": </span>
+                        <ul class="answer">
+                            <li>Email address: <strong>BearPass Email@missouristate.edu</strong></li>
+                            <li>User name: <strong>SGF/BearPass Login/BearPass Email</strong></li>
+                            <li>Password: <strong>Missouri State Password</strong></li>
+                            <li>Email server: <strong>bearmail.missouristate.edu</strong></li>
+                        </ul>
+                  </li>
+                        <li>Click "OK" and Bearmail should be configured.</li>
+                    </ol>
+                    <p class="indent">
+                    You may also configure your email accounts online by visiting: <a href="http://www.blackberry.com/emailsetup">http://www.blackberry.com/emailsetup</a>.
+                    Simply find the link for you specific provider at the bottom of the page in order to log in and configure your settings.
+                    </p>
+                    <p class="answer">
+                	If you have any difficulties with this tutorial, please feel free to
+                        <a href="/contact.php" title="contact us">contact</a> either the ResNet Offices (417 836-6100)
+                	or the open access computer labs (417 836-5891 option #4) for assistance.
+                    </p>
+                </div>
+            </div>
+
+
+
+
+
+            <div id="LeftNav">
+                <?php include_once($path . "/includes/LeftNav.php"); ?>
+            </div>
+            <?php include_once($path . "/includes/footer.inc.php"); ?>
+        </div>
+
+
+
+    </body>
+</html>

3a4ff2ca1d8c87ddc3af98baddb3d666492b4cfc
simxitnsz <seth_thoenen@hotmail.com>
2015-01-06 10:18:59 -0600

    Fixed the includes statements on the legal media sources page.

--- info/LegalMediaSources.php
+++ info/LegalMediaSources.php
@@ -1,161 +1,162 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<link rel="stylesheet" href="/css/reset.css" type="text/css" />
-<link rel="stylesheet" href="/css/stylesheet.css" type="text/css" />
-<link rel="stylesheet" href="/css/print.css" type="text/css" media="print" />
-<title>Resnet :: Legal Alternatives to File-sharing</title>
-</head>
-<body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="/css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="/css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="/css/print.css" type="text/css" media="print" />
+        <title>Resnet :: Legal Alternatives to File-sharing</title>
+    </head>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'];
+    ?>
+    <body>
 
-	<?php include_once("includes/navBar_primary.inc.php"); ?>
-		
-	<div id = "wrapper">
-	<?php include_once("includes/header_primary.inc.php"); ?>
+        <?php include_once($path . "/includes/navBar_primary.inc.php"); ?>
 
-			<!-- Content -->
-            <div id = "Main">
-                        		<!--start content-->		
-<h2>Legal Alternatives to File-sharing</h2>
-<p class="norm">
-Regardless of how wide-spread file-sharing is, it is illegal and violates the University's <a href="/info/aup.php">acceptable use policy</a>. 
-Why would you risk losing your internet and a possible lawsuit when there are many cheap and sometimes free alternatives to get your music, movies, and tv shows legally? 
-For your convenience, we have compiled a list of options to help you keep up on your shows and keep your mp3 player full.	
-</p>
-<p class="norm">
-It is also good to remember that the number of legal alternatives grows regularly, so doing a Google search to find what works for you may be your best option.  
-</p>
-<h3>Music Suggestions</h3>
-	<h2>Paid</h2>
-		<ul><li><h4><a href="http://www.amazon.com/">Amazon MP3</a></h4></li>
-			<p class="answer">
-			Amazon mp3 offers millions of songs and albums from artists from around the world. Most songs cost between 89 and 99 cents and most albums cost between $5.99 and $9.99. 
-			You are able to listen to 30 second clips of all the songs before you make a purchase. 
-			All of the songs are in mp3 format and they contain no DRM which means you can play them through any programs or any devices that supports mp3 playback, including iPods. 
+        <div id="wrapper">
+            <?php include_once($path . "/includes/header_primary.inc.php"); ?>
+
+            <!-- Content -->
+            <div id="Main">
+                <!--start content-->
+                <h2>Legal Alternatives to File-sharing</h2>
+                <p class="norm">
+Regardless of how wide-spread file-sharing is, it is illegal and violates the University's <a href="/info/aup.php">acceptable use policy</a>.
+Why would you risk losing your internet and a possible lawsuit when there are many cheap and sometimes free alternatives to get your music, movies, and tv shows legally?
+For your convenience, we have compiled a list of options to help you keep up on your shows and keep your mp3 player full.
+                </p>
+                <p class="norm">
+It is also good to remember that the number of legal alternatives grows regularly, so doing a Google search to find what works for you may be your best option.
+                </p>
+                <h3>Music Suggestions</h3>
+                <h2>Paid</h2>
+                <ul>
+                    <li><h4><a href="http://www.amazon.com/">Amazon MP3</a></h4></li>
+                    <p class="answer">
+			Amazon mp3 offers millions of songs and albums from artists from around the world. Most songs cost between 89 and 99 cents and most albums cost between $5.99 and $9.99.
+			You are able to listen to 30 second clips of all the songs before you make a purchase.
+			All of the songs are in mp3 format and they contain no DRM which means you can play them through any programs or any devices that supports mp3 playback, including iPods.
 			All purchases are permanent and Amazon offers cloud-based storage for purchased music.
-			</p>
-			<li><h4><a href="http://www.emusic.com">eMusic</a></h4></li>
-			<p class="answer">
-			eMusic is a great source for independent and hard to find music. 
-			They have over 2 million songs in their catalogue. 
-			Their songs are in mp3 format and contain no DRM which means you can play them through any programs or devices that support mp3 playback, including iPods. 
-			To use their service you must sign up for a subscription service where you pay a set amount each month and get a set number of free downloads. 
-			Plans start at $11.99/mo for 30 downloads a month to $24.99/mo for 100 downloads a month. 
+                    </p>
+                    <li><h4><a href="http://www.emusic.com">eMusic</a></h4></li>
+                    <p class="answer">
+			eMusic is a great source for independent and hard to find music.
+			They have over 2 million songs in their catalogue.
+			Their songs are in mp3 format and contain no DRM which means you can play them through any programs or devices that support mp3 playback, including iPods.
+			To use their service you must sign up for a subscription service where you pay a set amount each month and get a set number of free downloads.
+			Plans start at $11.99/mo for 30 downloads a month to $24.99/mo for 100 downloads a month.
 			These download are permanent.
-			</p>
-			<li><h4><a href="http://www.apple.com/itunes/">iTunes</a></h4></li>
-			<p class="answer">
-			iTunes is one of the most popular music downloading services. 
-			For only $1.29 per song you can get most of their over 28 million songs. 
-			All songs are playable on iPods. 
-			These downloads are permanent. 
-			</p>
-			<li><h4><a href="http://www.napster.com/">Napster</a></h4></li>
-			<p class="answer">
-			Napster, one of the most notable names in music downloading, offers two subscription plans.  
-			You can sign up for $9.99/mo to get unlimited downloads and ability to play songs online or through the Napster player, or for $14.95/mo you get the added ability to transfer to 'Play For Sure' devices. 
-			These songs do have DRM, so you can only play them through the Napster Player or online, and once you end your subscription your songs are no longer playable. 
-			Additionally, these songs are not compatible with iPods. 
-			</p>
-			<li><h4><a href="http://www.rhapsody.com/">Rhapsody</a></h4></li>
-			<p class="answer">
-			Rhapsody is a very popular service. 
-			It is similar to Napster in that you have unlimited downloading to your computer and streaming online for $9.99/mo with downloading to one mobile device, or $14.99/mo for downloading to three mobile devices. 
+                    </p>
+                    <li><h4><a href="http://www.apple.com/itunes/">iTunes</a></h4></li>
+                    <p class="answer">
+			iTunes is one of the most popular music downloading services.
+			For only $1.29 per song you can get most of their over 28 million songs.
+			All songs are playable on iPods.
+			These downloads are permanent.
+                    </p>
+                    <li><h4><a href="http://www.napster.com/">Napster</a></h4></li>
+                    <p class="answer">
+			Napster, one of the most notable names in music downloading, offers two subscription plans.
+			You can sign up for $9.99/mo to get unlimited downloads and ability to play songs online or through the Napster player, or for $14.95/mo you get the added ability to transfer to 'Play For Sure' devices.
+			These songs do have DRM, so you can only play them through the Napster Player or online, and once you end your subscription your songs are no longer playable.
+			Additionally, these songs are not compatible with iPods.
+                    </p>
+                    <li><h4><a href="http://www.rhapsody.com/">Rhapsody</a></h4></li>
+                    <p class="answer">
+			Rhapsody is a very popular service.
+			It is similar to Napster in that you have unlimited downloading to your computer and streaming online for $9.99/mo with downloading to one mobile device, or $14.99/mo for downloading to three mobile devices.
 			These songs are DRM Locked so you have to play them through the Rhapsody player or stream them and they are not compatible with iPods.
-			</p>	
-			<li><h4><a href="http://www.zune.com/">Zune</a></h4></li>
-			<p class="answer">
-			The Zune Marketplace is a necessity for Zune owners, but others can enjoy the 14 million+ songs as well. 
-			You can purchase DRM-free songs for $0.99 a piece, or you can buy a Zune Pass for $9.99/mo and enjoy unlimited downloads to your pc. 
-			Each month you get to keep 10 songs. 
-			The downloadable music is DRM-free songs and in mp3 format, so it can and can be used on any player or device (including iPods). 
+                    </p>
+                    <li><h4><a href="http://www.zune.com/">Zune</a></h4></li>
+                    <p class="answer">
+			The Zune Marketplace is a necessity for Zune owners, but others can enjoy the 14 million+ songs as well.
+			You can purchase DRM-free songs for $0.99 a piece, or you can buy a Zune Pass for $9.99/mo and enjoy unlimited downloads to your pc.
+			Each month you get to keep 10 songs.
+			The downloadable music is DRM-free songs and in mp3 format, so it can and can be used on any player or device (including iPods).
 			However, the songs not included in the 10 you get per month do have DRM included and can only be played with the Zune player and on your Zune device.
-			<li><h4><a href="https://play.google.com/music/listen">Google Play Music</a></h4></li>
-			<p class="answer">
-			With Google Play Music, you can buy individual songs for 1.29 and most albums range from 3.99 to 11.99.  
-			This music is DRM free and will automatically be added to your Google music cloud storage.  
+                    </p>
+                    <li><h4><a href="https://play.google.com/music/listen">Google Play Music</a></h4></li>
+                    <p class="answer">
+			With Google Play Music, you can buy individual songs for 1.29 and most albums range from 3.99 to 11.99.
+			This music is DRM free and will automatically be added to your Google music cloud storage.
 			You can also download this music to your computer or any mp3-capable device.
-			</p>
-		</ul><h2>Free Options</h2>
-		<ul><li><h4><a href="http://www.last.fm/">Last.fm</a></h4></li>
-			<p class="answer">
-			Last.fm is "the music service that learns what you love." 
-			There is no downloading with this service; instead, it just streams music to your comptuer, and the best part is that it is free. 
-			You simply type in a band or song that you like and it will generate a playlist with similar music that you might enjoy based on your listening history compared similar users. 
-			It also provides links to download the songs through iTunes.  
-			Additionally, there is an iPhone app available for it. 
-			</p>
-			<li><h4><a href="http://www.grooveshark.com/">Grooveshark</a></h4></li>
-			<p class="answer">
+                    </p>
+                </ul><h2>Free Options</h2>
+                <ul>
+                    <li><h4><a href="http://www.last.fm/">Last.fm</a></h4></li>
+                    <p class="answer">
+			Last.fm is "the music service that learns what you love."
+			There is no downloading with this service; instead, it just streams music to your comptuer, and the best part is that it is free.
+			You simply type in a band or song that you like and it will generate a playlist with similar music that you might enjoy based on your listening history compared similar users.
+			It also provides links to download the songs through iTunes.
+			Additionally, there is an iPhone app available for it.
+                    </p>
+                    <li><h4><a href="http://www.grooveshark.com/">Grooveshark</a></h4></li>
+                    <p class="answer">
 			Grooveshark is a free streaming service for music with some video commercials that has countless songs available, simply by searching.
 			Additionally, it can create a radiostation based off of the music you have picked previously.
 			You can also pay $6 a month to remove ads or $9 a month to play Grooveshark on your phone or other mobile device.
-			</p>
-			<li><h4><a href="http://www.iheart.com/">iHeartRadio</a></h4></li>
-			<p class="answer">
-			iHeartRadio is a free radio service that’s similar to Pandora or last.fm.  
-			You can listen to actual radio stations or create your own radio station based off an artist or song.  
+                    </p>
+                    <li><h4><a href="http://www.iheart.com/">iHeartRadio</a></h4></li>
+                    <p class="answer">
+			iHeartRadio is a free radio service that’s similar to Pandora or last.fm.
+			You can listen to actual radio stations or create your own radio station based off an artist or song.
 			At this time there are no ads on iHeartRadio.
-			</p>			
-			<li><h4><a href="http://www.pandora.com/">Pandora</a></h4></li>
-			<p class="answer">
-			Similar to Last.fm, Pandora is a streaming only service that will help suggest new songs based on <a href="http://en.wikipedia.org/wiki/Music_Genome_Project">Music Genome Project</a> and your preferences. 
-			It is supported by ads and recommending music for sale.  
-			It has mobile applications available on the iPhone and other devices. 
-			</p>
-		</ul><!--Begin Video options--><h1>Movie And Television Suggestions</h1>
-	<h3>Paid</h3>
-		<ul><li><h4><a href="http://www.cinemanow.com/">Cinema Now</a></h4></li>
-			<p class="answer">
+                    </p>
+                    <li><h4><a href="http://www.pandora.com/">Pandora</a></h4></li>
+                    <p class="answer">
+			Similar to Last.fm, Pandora is a streaming only service that will help suggest new songs based on <a href="http://en.wikipedia.org/wiki/Music_Genome_Project">Music Genome Project</a> and your preferences.
+			It is supported by ads and recommending music for sale.
+			It has mobile applications available on the iPhone and other devices.
+                    </p>
+                </ul><!--Begin Video options--><h1>Movie And Television Suggestions</h1>
+                <h3>Paid</h3>
+                <ul>
+                    <li><h4><a href="http://www.cinemanow.com/">Cinema Now</a></h4></li>
+                    <p class="answer">
 			Cinema Now has movies, TV shows and music videos available for download. You can download movies to own for $9.99 to $19.99, pay-per-view broadcasts are $2.99 to $3.99, and burn-to-DVD for $8.99 to $14.99.
 			TV shows and music videos are $1.99 to download.
 			Additionally, you can 'rent' movies for as little as $3.99 per title.
-			</p>
-			<li><h4><a href="http://www.amazon.com/">Amazon Instant Streaming</a></h4></li>
-			<p class="answer">
+                    </p>
+                    <li><h4><a href="http://www.amazon.com/">Amazon Instant Streaming</a></h4></li>
+                    <p class="answer">
 			Amazon offers a video streaming service with the upgrade to an Amazon Prime account.
-			It costs $79 a year, so it’s roughly 6.58 a month.  
-			With Prime you can stream video and “borrow” books from the kindle lender’s library.  
+			It costs $79 a year, so it’s roughly 6.58 a month.
+			With Prime you can stream video and “borrow” books from the kindle lender’s library.
 			You also get free two day shipping on all amazon orders.
-			</p>
-			<li><h4><a href="https://play.google.com/store/movies?hl=en">Google Play Movies</a></h4></li>
-			<p class="answer">
+                    </p>
+                    <li><h4><a href="https://play.google.com/store/movies?hl=en">Google Play Movies</a></h4></li>
+                    <p class="answer">
 			Google Play has a movie renting service in addition to music.
-			You can rent movies for between $2.99 and $3.99. 
+			You can rent movies for between $2.99 and $3.99.
 			You will have to stream these movies from Google Play.
-			</p>
-			<li><h4><a href="http://www.netflix.com/">Netflix</a></h4></li>
-			<p class="answer">
+                    </p>
+                    <li><h4><a href="http://www.netflix.com/">Netflix</a></h4></li>
+                    <p class="answer">
 			Netflix, aside from being a DVD rental service, has a streaming service that has many thousands of videos immediately available.
-			Plans for unlimited streaming start at as little as $7.99. 
+			Plans for unlimited streaming start at as little as $7.99.
 			They offer a two week free trial period to try out their services.
-			</p>
-		</ul><h3>Free Options</h3>
-		<ul><li><h4><a href="http://www.hulu.com/">Hulu</a></h4></li>
-			<p class="answer">
-			Hulu.com is an excellent web site that specializes in TV shows, but also features films and clips. 
-			They do not require you to sign up nor charge for their services.  
+                    </p>
+                </ul><h3>Free Options</h3>
+                <ul>
+                    <li><h4><a href="http://www.hulu.com/">Hulu</a></h4></li>
+                    <p class="answer">
+			Hulu.com is an excellent web site that specializes in TV shows, but also features films and clips.
+			They do not require you to sign up nor charge for their services.
 			Instead, videos contain limited commercial interuptions.
 			Hulu does now offer a pay service which provides full seasons of certain television shows.</p>
-		</ul><!-- end content -->
-
-                  </div>
+                </ul><!-- end content -->
+            </div>
 
             <!-- //end content -->
-		
-         	
-            
-
-	
-			<div id = "LeftNav">
-				<?php include_once("includes/LeftNav.php") ?>
-			</div>
-	<?php include_once("includes/footer.inc.php") ?>
-    </div>
+            <div id="LeftNav">
+                <?php include_once($path . "/includes/LeftNav.php") ?>
+            </div>
+            <?php include_once($path . "/includes/footer.inc.php") ?>
+        </div>
 
 
 
-</body>
+    </body>
 </html>

0f0b81df855eb5048975baabf3b8cfc375d22ded
smmehl0311 <smmehl0311@gmail.com>
2015-03-23 15:21:04 -0500

    changed doctype to html5 and added missing alt tag on img element

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
@@ -37,7 +37,7 @@
             <?php include_once($path . "/includes/header_primary.inc.php"); ?>
             <!-- <div id="homeCopy"></div> -->
             <div class="banner">
-                <img src="\images\banner.jpg" />
+                <img src="\images\banner.jpg" alt="banner.jpg"/>
             </div>
             <div class="homeTutorialWrapper">
                 <div class="Welcome">

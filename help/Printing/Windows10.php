c8b2bce54d3f07314f2a8f7b5c7c9a297a979700
msuresnet <msuresnet@users.noreply.github.com>
2016-05-23 15:56:13 -0500

    Create Windows10.php

---
+++ help/Printing/Windows10.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="/css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="/css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="/css/print.css" type="text/css" media="print" />
+        <script type="text/javascript" src="/js/jquery-1.4.2.js"></script>
+        <script type="text/javascript" src="/js/slickBox.js"></script>
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-2196753-3', 'missouristate.edu');
+            ga('send', 'pageview');
+            
+        </script>
+
+        <title>Resnet :: Tutorials :: Printer Setup</title>
+    </head>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'];
+    ?>
+    <body>
+        <?php include_once($path . "/includes/navBar_primary.inc.php"); ?>
+
+        <div id="wrapper">
+            <?php include_once($path . "/includes/header_primary.inc.php"); ?>
+
+            <div id="Main">
+                <!-- start content -->
+                <div id="tutorial">
+                  <h3><a href="#WIN10PRINT" name="WIN10PRINT"> Setting up the front desk printer on Windows 10.</a></h3>
+                    <p class="answer"> 
+                      This will guide you through the setup of the front desk printer in your residence hall on a computer running Windows 10.<br />
+                    </p>
+                      </p>
+                    <ol clas="indent">
+                      <li>
+                        Click the Start Button and type in the search bar <strong>\\resprint</strong> and hit enter.
+                      </li>
+                        <img class="imgBorder" src="/images/printerSetup/Windows8/printer-win10-1.png" alt="start menu" />
+                      <li>
+                        Now login with your <strong>BearPass ID</strong>
+                      </li>
+                        <img class="imgBorder" src="/images/printerSetup/Windows8/printer-win10-2.png" alt="log-in" />
+                      <li>
+                        Next, select your Residence Hall's front desk printer, right click it and click connect.
+                      </li>
+                        <img class="imgBorder" src="/images/printerSetup/Windows8/printer-win10-3.png" alt="log-in" />
+                      <li>
+                        You should see this screen with the name of the front desk printer you chose. Once this finishes you will be ready to print.
+                      </li>
+                        <img class="imgBorder" src="/images/printerSetup/Windows8/printer-win10-4.png" alt="log-in" />
+                      </ol>
+                      <p class="answer"> 
+                        If you have any difficulties with this tutorial, please feel freee to 
+                        <a href="/contact.php" title="contact us"> contact</a> the ResNet Offices (417-836-6100) for assistance.
+                      </p>
+                      <!-- end content -->
+                    </div>
+                    <div id="LeftNav">
+                      <?php indlude_once($path . "/includes/LeftNav.php") ?>
+                    </div>
+                    <?php include_once($path . "/includes/footer.inc.php") ?>
+                  </div>
+                </body>
+              </html>

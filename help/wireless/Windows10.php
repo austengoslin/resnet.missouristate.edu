dfbdd6ef80516e347fe91fbf3f2d29bd63d30e69
msuresnet <msuresnet@users.noreply.github.com>
2016-05-23 15:25:41 -0500

    Create Windows10.php

---
+++ help/wireless/Windows10.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <link rel="stylesheet" href="/css/reset.css" type="text/css" />
+        <link rel="stylesheet" href="/css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="/css/print.css" type="text/css" media="print" />
+        <script type="text/javascript" src="/js/jquery-1.4.2.js"></script>
+        <script type="text/javascript" src="/js/slickBox.js"></script>
+
+        <title>Resnet :: Tutorials :: Wireless :: Configuring Resnet 40 on Windows 8</title>
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            
+            ga('create', 'UA-2196753-3', 'missouristate.edu');
+            ga('send', 'pageview');
+            
+        </script>
+    </head>
+    <?php
+        $path = $_SERVER['DOCUMENT_ROOT'];
+    ?>
+    <body>
+
+
+        <?php include_once($path . "/includes/navBar_primary.inc.php") ?>
+
+        <div id="wrapper">
+            <?php include_once($path . "/includes/header_primary.inc.php") ?>
+
+            <div id="Main">
+
+                <div id="tutorial">
+                    <!-- start content -->
+                    <h2>Configuring Network on Windows 10</h2>
+                    <ol class="indent">
+                        <li>
+					At the Desktop, click on the WiFi Icon at the bottom right of your screen.
+                        </li>
+                        <img src="/images/wireless/wireless-win10-01.png" alt="wireless icon" class="imgBorder" width="575" />
+                        <li>
+					Once you click on it, you should see a list of your available wireless networks. Select <strong>MSU ResNet</strong> if you live On-Campus and <strong>MSU</strong> if you live Off-Campus then click connect.
+                        </li>
+                        <img src="/images/wireless/wireless-win10-2.png" alt="available networks" class="imgBorder" />
+                        <li>
+				Once here you will be prompted with a login screen. Enter you <strong>BearPass ID</strong> as your User name and your <strong>BearPass Password</strong> as your Password and click OK.
+                        </li>
+                        <img src="/images/wireless/wireless-win10-3.png" alt="Credentials" class="imgBorder" />
+                    </ol>
+                    <p class="answer">
+                                If you have any difficulties with this tutorial, please feel free to
+                        <a href="/contact.php" title="contact us">contact</a> either the ResNet Offices (417 836-6100)
+                                or the open access computer labs (417 836-4519) for assistance.
+                    </p>
+
+
+
+                    <!-- end content -->
+                </div>
+            </div>
+            <div id="LeftNav">
+                <?php include_once($path . "/includes/LeftNav.php") ?>
+            </div>
+            <?php include_once($path . "/includes/footer.inc.php") ?>
+        </div>
+
+
+
+    </body>
+</html>
